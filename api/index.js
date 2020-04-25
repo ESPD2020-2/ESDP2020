00f7c82d3d950a9db5a6f6d833f64ffa4a6926c6
aliyatte <a.akmatova@gmail.com>
2020-04-25 18:37:43 +0600

    Added dependencies and basic settings

--- api/index.js
+++ api/index.js
@@ -1,1 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
 const port = 8000;
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static('public'));
+
+const run = async () => {
+  app.listen(port, () => {
+    console.log(`HTTP Server started on ${port} port!`);
+  });
+};
+
+run().catch(error => {
+  console.error(error);
+});
