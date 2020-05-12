77e0d45c34d5a216c33b1310d9f57809d529c7ef
Baktiyar Ishenov <b1963826@gmail.com>
2020-05-13 00:18:08 +0600

    #10 примерная Верстка страницы Контакты

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,6 +3,8 @@ import {Route, Switch} from "react-router-dom";
 import Register from "./containers/Register/Register";
 import Login from "./containers/Login/Login";
 import Orders from './containers/Orders/Orders';
+import Contacts from "./containers/Contacts/Contacts";
+import Faq from "./containers/FAQ/FAQ";
 
 const Routes = () => {
     return (
@@ -10,6 +12,8 @@ const Routes = () => {
             <Route path="/register" exact component={Register}/>
             <Route path="/login" exact component={Login}/>
             <Route path="/orders" exact component={Orders}/>
+            <Route path="/contacts" exact component={Contacts}/>
+            <Route path="/faq" exact component={Faq}/>
         </Switch>
     );
 };
