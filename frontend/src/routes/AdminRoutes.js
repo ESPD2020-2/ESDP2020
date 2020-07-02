c1e68ad51930b30318b16ff3d62908b57d9bd46b
Aman Chermashev <bakytaman228337@gmail.com>
2020-07-02 18:58:42 +0600

    Page 404 admin layout debug

--- frontend/src/routes/AdminRoutes.js
+++ frontend/src/routes/AdminRoutes.js
@@ -14,7 +14,7 @@ const AdminRoutes = ({path}) => {
             <Route path={`${path}/orders/created`} exact component={Orders}/>
             <Route path={`${path}/orders/published`} exact component={Orders}/>
             <Route path={`${path}/orders/accepted`} exact component={Orders}/>
-            <Route path={`${path}/orders/courier/accepted`} component={Orders}/>
+            <Route path={`${path}/orders/courier/accepted`} exact component={Orders}/>
             <Route path={`${path}/users`} exact component={Users}/>
             <Route path={`${path}/trecking`} exact component={CourierTrecking}/>
             <Route path={`${path}/`} exact component={Dashboard}/>
