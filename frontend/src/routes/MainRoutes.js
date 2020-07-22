41732d3c89cbfd5c6c34c555055d29dfd77f5523
Aman Chermashev <bakytaman228337@gmail.com>
2020-07-22 14:56:01 +0600

    Изменил страницу с вопросами и убрал ссылку на страницу для курьеров

--- frontend/src/routes/MainRoutes.js
+++ frontend/src/routes/MainRoutes.js
@@ -1,12 +1,12 @@
 import React from "react";
 import {Route, Switch} from "react-router-dom";
 import NewOrder from "../containers/NewOrder/NewOrder";
-import FAQ from "../containers/FAQ/FAQ";
 import Contacts from "../containers/Contacts/Contacts";
 import About from "../containers/About/About";
-import ForCouriers from "../containers/ForCouriers/forCouriers";
 import ReviewForm from "../components/ReviewForm/ReviewForm";
 import NotFound from "../containers/NotFound/NotFound";
+import FAQ from "../containers/FAQ/FAQ";
+
 
 const Routes = () => {
     return (
@@ -15,7 +15,6 @@ const Routes = () => {
             <Route path="/orders/:id/edit" exact component={NewOrder}/>
             <Route path="/faq" exact component={FAQ}/>
             <Route path="/contacts" exact component={Contacts}/>
-            <Route path="/couriers" exact component={ForCouriers}/>
             <Route path='/about' exact component={About}/>
             <Route path='/review-form' exact component={ReviewForm}/>
             <Route path='/' exact render={() => <h1>Main Page</h1>}/>
